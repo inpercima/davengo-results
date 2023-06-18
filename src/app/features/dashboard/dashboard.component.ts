c896771e2361050a8e0ee51ecc186a01d8f1b917
Marcel Jänicke <inpercima@gmail.com>
2023-06-18 07:54:31 +0200

    Prepare display of runs

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 import { DavengoService } from '../../core/davengo.service';
+import { YearRanking } from 'src/app/core/ranking.model';
+import { NgIf } from '@angular/common';
 
 @Component({
+  imports: [
+    MatCardModule,
+    MatTableModule,
+    NgIf,
+  ],
   selector: 'dr-dashboard',
-  templateUrl: './dashboard.component.html',
   standalone: true,
+  templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent implements OnInit {
+
+  dataSource = new MatTableDataSource<YearRanking>();
+  displayedColumns: string[] = ['column1', 'column2', 'column3'];
+
+
   constructor(private dashboardService: DavengoService) {}
 
   ngOnInit(): void {
-    this.dashboardService.getRuns().subscribe((x) => console.log(x));
+    this.dashboardService.getRuns().subscribe((x) => this.dataSource = new MatTableDataSource(x));
   }
 }
