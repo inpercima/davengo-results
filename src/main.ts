1217b81a0cbc29678f5a9d136ab479919298d00b
Marcel Jänicke <inpercima@gmail.com>
2023-07-05 15:05:11 +0200

    Using ApplicationConfig

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
-import { OverlayModule } from '@angular/cdk/overlay';
-import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule, OverlayModule), provideAnimations(), provideHttpClient()],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
